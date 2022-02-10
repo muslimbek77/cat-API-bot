ef997131fe592955660dc6633256fd4ca83eb5cf
Muslimbek <48023644+muslimbek77@users.noreply.github.com>
2022-02-10 21:49:50 +0500

    Update mushuk.py

--- handlers/users/mushuk.py
+++ handlers/users/mushuk.py
@@ -2,15 +2,11 @@ from aiogram.types.message import Message
 import requests
 from loader import dp
 
-def get_url():
-    contents = requests.get('https://thatcopy.pw/catapi/rest/').json()
-    image_url = contents['url']
-    return image_url
+
  
 # Echo bot
 @dp.message_handler(state=None, commands='mushuk')
 async def bot_echo(message: Message):
-
-    url = get_url()    
-    await message.answer_photo(url,caption="<a href='https://t.me/muizzabot'>Mushuklar</a> 🐈 ")
+    await message.answer_photo((requests.get('https://api.thecatapi.com/v1/images/search').json()[0]['url'])
+,caption="<a href='https://t.me/muizzabot'>Mushuklar</a> 🐈 ")
 
