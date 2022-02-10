f3ccffc92b8c7e45f233d84f14f3764658e2be4f
Muslimbek <48023644+muslimbek77@users.noreply.github.com>
2022-02-10 21:53:02 +0500

    Update mushuk.py

--- handlers/users/mushuk.py
+++ handlers/users/mushuk.py
@@ -7,6 +7,6 @@ from loader import dp
 # Echo bot
 @dp.message_handler(state=None, commands='mushuk')
 async def bot_echo(message: Message):
-    await message.answer_photo((requests.get('https://api.thecatapi.com/v1/images/search').json()[0]['url'])
-,caption="<a href='https://t.me/muizzabot'>Mushuklar</a> 🐈 ")
+  url= requests.get('https://api.thecatapi.com/v1/images/search').json()[0]['url']
+  await message.answer_photo(url,caption="<a href='https://t.me/muizzabot'>Mushuklar</a> 🐈 ")
 
